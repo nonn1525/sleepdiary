47bb391c7d2ec7256fcb47110ed0297d782a9a93
Chisato Kato <80097516+nonn1525@users.noreply.github.com>
2021-09-09 10:32:46 +0900

    settingまで

---
+++ screens/DiaryScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const DiaryScreen = () => {
+  return (
+    <View>
+      <Text>ダイアリーページ</Text>
+    </View>
+  );
+};
+export default DiaryScreen;
