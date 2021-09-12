201b7a41c7562f4fa6a46dc18bae67dd51664840
Chisato Kato <80097516+nonn1525@users.noreply.github.com>
2021-09-12 10:23:45 +0900

    Update CalendarComponent.jsx

--- components/CalendarComponent.jsx
+++ components/CalendarComponent.jsx
@@ -4,7 +4,6 @@ import { View, Text } from "react-native";
 
 export default CalendarComponent = () => {
   const [sample, setSample] = useState({
-    customStyles: { text: { fontSize: 5 } },
     "2021-09-12": "\n睡眠時間:12\n起きやすさ:3\n元気度:5",
     "2021-09-13": "\n睡眠時間:13\n起きやすさ:4\n元気度:4",
   });
@@ -14,9 +13,6 @@ export default CalendarComponent = () => {
       <Calendar
         monthFormat={"yyyy年 MM月"}
         markedDates={sample}
-        // "2021-09-12": aa[0],
-        // "2021-09-13": aa[1],
-        // }
         dayComponent={({ date, state, marking }) => {
           return (
             <View>
@@ -28,7 +24,6 @@ export default CalendarComponent = () => {
                 onPress={() => alert(date.day)}
               >
                 {date.day}
-                {/* {marking} */}
               </Text>
               <Text
                 style={{
